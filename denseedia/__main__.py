92f3d419761ffb01e5f830ffa0773322921d9cfd
Fabien Sabre <fabien.sabre@gmail.com>
2022-01-19 19:22:22 +0100

    Allow to start the FastAPI server with a command

--- denseedia/__main__.py
+++ denseedia/__main__.py
@@ -1,8 +1,5 @@
 """DenseEdia module endpoint"""
 
-from .api.launch import launch_server
-from .constants import DEFAULT_FILE_NAME, ROOT_PATH
-from .storage.tables import use_database
+from .cli.cli import main_group
 
-use_database(ROOT_PATH / DEFAULT_FILE_NAME)
-launch_server()
+main_group()
