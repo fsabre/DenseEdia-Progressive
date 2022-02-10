d79a5e8adb93cb1d0f5c7fa4fcfccff0242f22bc
Fabien Sabre <fabien.sabre@gmail.com>
2022-02-10 21:37:29 +0100

    Allow all origins with CORS

--- denseedia/api/app.py
+++ denseedia/api/app.py
@@ -3,11 +3,20 @@
 from typing import List
 
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 
 from . import operations
 from .. import exceptions, models
 
 app = FastAPI(title="DenseEdia")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get(
