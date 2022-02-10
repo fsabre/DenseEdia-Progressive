3613c71764f3dddd04348a8b4e3073333012059e
Fabien Sabre <fabien.sabre@gmail.com>
2022-02-10 21:29:56 +0100

    Change the API port to 59130

--- denseedia/api/launch.py
+++ denseedia/api/launch.py
@@ -3,8 +3,10 @@
 import uvicorn
 
 from .app import app
+from ..constants import API_PORT
 
 
 def launch_server() -> None:
     """Run the FastApi server."""
-    uvicorn.run(app)
+    print(f"Documentation page at http://localhost:{API_PORT}/docs")
+    uvicorn.run(app, port=API_PORT)
--- denseedia/constants.py
+++ denseedia/constants.py
@@ -4,3 +4,4 @@ from pathlib import Path
 
 ROOT_PATH = Path(__file__).parent.parent
 DEFAULT_FILE_NAME = "db.db"
+API_PORT: int = 59130
