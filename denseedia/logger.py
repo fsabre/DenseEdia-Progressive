63503741afdec7be8ceff4702bef76735695dcec
Fabien Sabre <fabien.sabre@gmail.com>
2022-06-11 20:08:55 +0200

    feat: Improve the ponyORM logging

--- denseedia/logger.py
+++ denseedia/logger.py
@@ -2,9 +2,25 @@
 
 import logging
 
+formatter = logging.Formatter("%(asctime)s::%(levelname)s::%(message)s")
+stream_handler = logging.StreamHandler()
+stream_handler.setFormatter(formatter)
+
 logger = logging.getLogger("denseedia")
 logger.setLevel(logging.WARNING)
+logger.addHandler(stream_handler)
 
-stream_h = logging.StreamHandler()
-stream_h.setFormatter(logging.Formatter("%(levelname)s\t%(message)s"))
-logger.addHandler(stream_h)
+
+class NoNewlineStreamHandler(logging.StreamHandler):
+    """Replace the newlines by spaces. Made to fix PonyORM logger."""
+
+    def emit(self, record):
+        record.msg = record.msg.replace("\n", " ")
+        super().emit(record)
+
+
+pony_logger = logging.getLogger("pony.orm")
+pony_logger.setLevel(logging.DEBUG)
+no_newline_steam_handler = NoNewlineStreamHandler()
+no_newline_steam_handler.setFormatter(formatter)
+pony_logger.addHandler(no_newline_steam_handler)
